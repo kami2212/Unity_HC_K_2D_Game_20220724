3eb818756e763d19d84871ee7f25b9a65d7edf44
kami2212 <110316254+kami2212@users.noreply.github.com>
2022-08-14 13:08:29 +0800

    程式frst

--- Unity_HC_K_2D_Game_20220724/Assets/Script/FirstScript.cs
+++ Unity_HC_K_2D_Game_20220724/Assets/Script/FirstScript.cs
@@ -1,6 +1,30 @@
-using UnityEngine;
-
-public class FirstScript : MonoBehaviour
+﻿using UnityEngine;
+namespace Su
 {
+    public class FirstScript : MonoBehaviour
+    {   /// <summary>
+        /// 開始事件
+        /// </summary>
+        #region //程式區塊
+        
+        private void Start()
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                print("うるさい～～");
+                i++;
+            }
+        }
+        /// <summary>
+        /// 更新事件
+        /// </summary>
+        private void Update()
+        {
+            print("ばか　へんたい　うるさい　もしらない");
 
+        }
+        #endregion
+    }
 }
+
---
+++ Unity_HC_K_2D_Game_20220724/Assets/Script/Learnfield.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Su
+{   /// <summary>
+    /// ¾Ç²ßÄæ¦ì
+    /// </summary>
+    public class Learnfield : MonoBehaviour
+    {
+        int number;
+
+        public int Carcc = 1500;
+        public float weight = 3.5f;
+        public string brand = "BMW";
+        public bool hasSkyWindow = true;
+
+        [Tooltip("§ðÀ»¤O")]
+        public int attack = 1500;
+    }
+}  
+
